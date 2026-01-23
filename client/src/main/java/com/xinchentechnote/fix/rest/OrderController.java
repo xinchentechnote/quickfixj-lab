2b50a38b02b8e7f6c625a9073386845a1ebd5008
xinchentechnote <283235702@qq.com>
2026-01-23 22:56:19 +0800

    feat: integrate quarkus

---
+++ client/src/main/java/com/xinchentechnote/fix/rest/OrderController.java
@@ -0,0 +1,15 @@
+package com.xinchentechnote.fix.rest;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/order")
+public class OrderController {
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello RESTEasy";
+  }
+}
